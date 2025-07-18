36601e283c393cbf65176a4a1f219dd90fee684c
Umer Farooque <mohranvi4484@gmail.com>
2025-07-18 17:18:54 +0500

    Update app.js

--- app.js
+++ app.js
@@ -124,7 +124,7 @@
 // Chapter 05
 
 // Q 01
-var num1 = 3;
-var num2 = 5;
-var sum = num1 + num2;
-document.writeln("<p>Sum of " + num1 + " and " + num2 + " is " + sum + "</p>");
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1 + num2;
+// document.writeln("<p>Sum of " + num1 + " and " + num2 + " is " + sum + "</p>");
