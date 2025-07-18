f1f5a1c5ce46007cca7829f1cc02ac8e318d8b86
Umer Farooque <mohranvi4484@gmail.com>
2025-07-18 19:38:22 +0500

    Add variable operation example to app.js
    Added code demonstrating pre- and post-increment/decrement operations on variables a and b, and outputting the result to the document.

--- app.js
+++ app.js
@@ -279,3 +279,10 @@
 // document.writeln("<p>Now the value of a is: " + a + " </p>");
 
 
+// Q 02
+var a = 2, b = 1;
+document.writeln("<p>a is: " + a + " </p>");
+document.writeln("<p>b is: " + b + " </p>");
+var result = --a - --b + ++b + b--;
+document.writeln("<p>Result is: " + result + " </p>");
+
