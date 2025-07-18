819d48c26d65bd9226015f0a1a0b656568afebf8
Umer Farooque <mohranvi4484@gmail.com>
2025-07-18 15:44:05 +0500

    Add alert for JS Land welcome message
    Introduced Q03 with an alert displaying 'Welcome to JS Land... Happy Coding!' to enhance user onboarding experience.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // Chapter 01
 
 // Q 01
-
 // alert("Welcome to my site!");
 
-// Q 02
 
+// Q 02
 // alert("Error! Please enter the correct password");
+
+
+// Q 03
+alert("Welcome to JS Land... \n Happy Coding!");
+
 
