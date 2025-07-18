d51af2a244a44e07f712a715fe0cd9ffdf6d5fef
Umer Farooque <mohranvi4484@gmail.com>
2025-07-18 19:38:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -280,9 +280,9 @@
 
 
 // Q 02
-var a = 2, b = 1;
-document.writeln("<p>a is: " + a + " </p>");
-document.writeln("<p>b is: " + b + " </p>");
-var result = --a - --b + ++b + b--;
-document.writeln("<p>Result is: " + result + " </p>");
+// var a = 2, b = 1;
+// document.writeln("<p>a is: " + a + " </p>");
+// document.writeln("<p>b is: " + b + " </p>");
+// var result = --a - --b + ++b + b--;
+// document.writeln("<p>Result is: " + result + " </p>");
 
