60508fefdec61202e18b36e89d7effe13921fd66
Umer Farooque <mohranvi4484@gmail.com>
2025-07-19 12:42:12 +0500

    Add secret number guessing game
    Introduced a simple guessing game where the user is prompted to guess a secret number between 1 and 10. Alerts are shown for correct guesses and near misses.

--- app.js
+++ app.js
@@ -446,5 +446,14 @@
 // }
 
 
+// Q 07
+var secretNumber = 7;
+var userGuess = +prompt("Guess the secret number (between 1 and 10):");
+if (userGuess === secretNumber) {
+    alert("Bingo! Correct answer.");
+}
+if (userGuess === secretNumber + 1) {
+    alert("Close enough to the correct answer.");
+}
 
 
