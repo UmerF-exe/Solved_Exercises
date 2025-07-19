4061d7cb554f9fd88d8bc6bc18b8c4ff914801ae
Umer Farooque <mohranvi4484@gmail.com>
2025-07-19 17:04:47 +0500

    Update app.js

--- app.js
+++ app.js
@@ -531,16 +531,19 @@
 
 
 // Q 02
-var num1 = +prompt("Enter first number:");
-var num2 = +prompt("Enter second number:");
-if (num1 > num2) {
-    alert(num1 + " is greater than " + num2);
-}
-else if (num1 < num2) {
-    alert(num2 + " is greater than " + num1);
-} else {
-    alert("Both numbers are equal.");
-}
+// var num1 = +prompt("Enter first number:");
+// var num2 = +prompt("Enter second number:");
+// if (num1 > num2) {
+//     alert(num1 + " is greater than " + num2);
+// }
+// else if (num1 < num2) {
+//     alert(num2 + " is greater than " + num1);
+// } else {
+//     alert("Both numbers are equal.");
+// }
+
+
+
 
 
 
