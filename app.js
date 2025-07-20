8da589eb4b7884fcf9abc2c3ad4b9c8eef6a00a2
Umer Farooque <mohranvi4484@gmail.com>
2025-07-20 14:43:48 +0500

    Q 14

--- app.js
+++ app.js
@@ -1048,3 +1048,19 @@
 // }
 
 
+// Q 14
+var arr =  ["cake", "apple pie", "cookie", "chips", "patties"];
+var found = false;
+var userInput = prompt("What do you want to buy / order on our bakery, dear Sir / Ma'am: ").toLowerCase();
+for(var i = 0; i < arr.length; i++){
+    if(userInput === arr[i]){
+        found = true;
+        break;
+    }
+}
+if(found){
+    alert("Yes! " + userInput + " is available at index " + arr.indexOf(userInput) + " in our bakery." );
+}
+else{
+    alert("Sorry! " + userInput + " is not available in our bakery." );
+}
