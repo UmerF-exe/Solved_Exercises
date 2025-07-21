5df81f9ce1211b28d7635880cd9ccc78e1273673
Umer Farooque <mohranvi4484@gmail.com>
2025-07-21 18:41:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1193,13 +1193,13 @@
 
 
 // Q 04
-var currrentDate = new Date();
-var days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
-var today = days[currrentDate.getDay()];
-if(today === "Sun" || today === "Sat"){
-    document.writeln("<h1>It's Fun Day</h1>");
-}
-else{
-    alert("Today is: " + today);
-}
+// var currrentDate = new Date();
+// var days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
+// var today = days[currrentDate.getDay()];
+// if(today === "Sun" || today === "Sat"){
+//     document.writeln("<h1>It's Fun Day</h1>");
+// }
+// else{
+//     alert("Today is: " + today);
+// }
 
